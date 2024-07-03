b04e85d08b8de6def642fd7bc4ac13e35e4f6724
Maher  Bouidani <maher.buedani@gmail.com>
2024-07-03 21:39:38 +0300

    implement get contract by id and fix it

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
+const { sequelize } = require("./model");
+const { Op } = require("sequelize");
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
+
+
 /**
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.get("/contracts/:id", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const { id } = req.params;
+  const { profile } = req;
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      [Op.or]: [{ clientId: profile.id }, { contractorId: profile.id }],
+    },
+  });
+  if (!contract) return res.status(404).end();
+  res.json(contract);
+});
+
+
 module.exports = app;
