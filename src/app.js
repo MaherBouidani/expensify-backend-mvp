db7140aeb5f1ad429a4bbce0802e5bb0e97a1376
Maher  Bouidani <maher.buedani@gmail.com>
2024-07-04 05:42:20 +0300

    feat: client pays for a job

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { sequelize } = require("./model");
+const { sequelize, Contract } = require("./model");
 const { Op } = require("sequelize");
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
@@ -71,6 +71,69 @@ app.get("/jobs/unpaid", getProfile, async (req, res) => {
     res.json(unpaidJobs);
   } catch (error) {
     console.error("Error retrieving unpaid jobs:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/jobs/:job_id/pay", getProfile, async (req, res) => {
+  const { Job, Profile } = req.app.get("models");
+  const { profile } = req;
+  const { job_id } = req.params;
+  const t = await sequelize.transaction();
+
+  try {
+    const transactionOptions = {transaction: t, lock: t.LOCK.UPDATE};
+
+    // const balance = profile.balance;
+    const job = await Job.findOne({
+      where: {
+        id: job_id
+      }, include: [Contract], ...transactionOptions
+    });
+
+    if (!job) {
+      return res.status(404).json({ error: "Job not found" });
+    }
+
+    if (profile.type != "client"){
+        return res.status(403).json({ error: "Unauthorized, Profile is not of client type" });
+    } 
+
+    if (profile.id !== job.Contract.ClientId){
+        return res.status(403).json({ error: "Unauthorized, Job does not belong to profile" });
+    }
+
+    if (job.paid) { 
+        return res.status(400).json({ error: "Job already paid" });
+    }
+
+    const client = await Profile.findOne({where: {id: job.Contract.ClientId}, ...transactionOptions});
+    const contractor = await Profile.findOne({where: {id: job.Contract.ContractorId}, ...transactionOptions});
+
+    if (!client || !contractor){
+        return res.status(404).json({ error: "Profile not found" });
+    }
+
+    if (client.balance < job.price) {
+        return res.status(400).json({ error: "Insufficient funds" });
+    }
+
+    client.balance -= job.price;
+    contractor.balance += job.price;
+
+    await client.save(transactionOptions);
+    await contractor.save(transactionOptions);
+
+    job.paid = true;
+    job.paymentDate = new Date();
+    await job.save(transactionOptions);
+
+    await t.commit();
+    res.json({ message: 'Job paid successfully' });
+
+  } catch (error) {
+    await t.rollback();
+    console.error("Error paying job:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
